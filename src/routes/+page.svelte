8bfc76bdcbe6e92160da45fa79ac8e2d5b984297
alexcoder04 <alexcoder04@protonmail.com>
2025-01-19 23:44:18 +0100

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<h1>athocs webUI (now in Svelte!)</h1>
+<h3>(work in progress)</h3>
+
+<ul>
+	<li>For CSV logs, use the API (<code>http://192.168.0.86:3000/api/data?station=RPIZ-ALEX&amp;time_from=...&amp;time_to=...</code>)</li>
+	<li><a href="/graphs/graph-last-24-hours.png" rel="external">Graph for last 24 hours</a></li>
+	<li><a href="/graphs/graph-all-2501171524.png" rel="external">Graph for all data until 2025-01-17 15:24</a></li>
+</ul>
+
