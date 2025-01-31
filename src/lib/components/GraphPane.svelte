73766af3b6ee4893c3b8360ca64573c466d15fc3
alexcoder04 <alexcoder04@protonmail.com>
2025-01-31 22:08:19 +0100

    auto-reload graphs every 5 mins

--- src/lib/components/GraphPane.svelte
+++ src/lib/components/GraphPane.svelte
@@ -1,39 +1,70 @@
 <script>
+    import { onDestroy, onMount } from "svelte";
     import { DEPLOYMENT_IP, DEPLOYMENT_PORT } from "../index.js";
-    let station = {
-        selected: "RPIZ-ALEX",
-        options: [
-            { value: "RPIZ-ALEX", label: "RPIZ-ALEX" }
-        ]
+
+    let paneData = {
+        image: {
+            src: null,
+            reloadInterval: 5,
+            interval: null
+        },
+        station: {
+            selected: "RPIZ-ALEX",
+            options: [
+                { value: "RPIZ-ALEX", label: "RPIZ-ALEX" }
+            ]
+        },
+        timePeriod: {
+            selected: "last-24h",
+            options: [
+                { value: "last-24h", label: "Last 24 hours" }
+            ]
+        }
     };
 
     function handleStationChange(event) {
-      station.selected = event.target.value;
+      paneData.station.selected = event.target.value;
     }
-
-    let timePeriod = {
-        selected: "last-24h",
-        options: [
-            { value: "last-24h", label: "Last 24 hours" }
-        ]
-    };
 
     function handleTimePeriodChange(event) {
-      timePeriod.selected = event.target.value;
+      paneData.timePeriod.selected = event.target.value;
     }
+
+    function getImageSrc() {
+        return `http://${DEPLOYMENT_IP}:${DEPLOYMENT_PORT}/graphs/graph-last-24-hours.png?t=${Date.now() % 1000000}`
+    }
+
+    function updateReloader() {
+        if (paneData.image.interval != null) {
+            clearInterval(paneData.image.interval);
+        }
+
+        paneData.image.interval = setInterval(() => {
+            paneData.image.src = getImageSrc();
+        }, 1000 * 60 * paneData.image.reloadInterval);
+    }
+
+    onMount(() => {
+        paneData.image.src = getImageSrc();
+        updateReloader();
+    });
+
+    onDestroy(() => {
+        clearInterval(paneData.image.interval);
+    });
 </script>
 
-<div class="w-full xl:w-1/2 p-2">
+<div class="w-full xl:w-1/2 p-2 flex-col container">
     <div class="bg-slate-600 text-white p-4 container flex items-center space-x-4">
-        <div class="flex items-center text-lg bg-slate-800 text-white py-1 px-2">
+        <div class="flex items-center text-lg bg-slate-800 py-1 px-2">
             <label for="station-dropdown">📡 </label>
             <select
                 id="station-dropdown"
                 class="block px-2"
-                bind:value={station.selected}
+                bind:value={paneData.station.selected}
                 on:change={handleStationChange}
             >
-                {#each station.options as { value, label }}
+                {#each paneData.station.options as { value, label }}
                     <option value={value}>{label}</option>
                 {/each}
             </select>
@@ -44,17 +75,17 @@
             <select
                 id="timeperiod-dropdown"
                 class="block px-2"
-                bind:value={timePeriod.selected}
+                bind:value={paneData.timePeriod.selected}
                 on:change={handleTimePeriodChange}
             >
-                {#each timePeriod.options as { value, label }}
+                {#each paneData.timePeriod.options as { value, label }}
                     <option value={value}>{label}</option>
                 {/each}
             </select>
         </div>
     </div>
 
-    <div>
-        <img src="http://{DEPLOYMENT_IP}:{DEPLOYMENT_PORT}/graphs/graph-last-24-hours.png" alt="graph of last 24 hours">
-    </div>
+    <a href={paneData.image.src} target="_blank">
+        <img src={paneData.image.src} alt="graph of last 24 hours">
+    </a>
 </div>
